206daf25ef904097ac49533d4f2e97e9e1a9ef32
MHAE391 <modatherelgohary391@gmail.com>
2022-12-16 15:21:24 +0200

    Update Testes

--- app/src/main/java/com/udacity/project4/utils/Constants.kt
+++ app/src/main/java/com/udacity/project4/utils/Constants.kt
@@ -1,5 +1,9 @@
 package com.udacity.project4.utils
 
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+
 object Constants {
     const val GEOFENCE_EVENT = "GEOFENCE EVENT"
     const val GEOFENCE_RADIUS = 15f
@@ -8,4 +12,15 @@ object Constants {
     const val FOREGROUND_REQUEST_CODE = 22
     const val LOCATION_PERMISSION_INDEX = 0
     const val BACKGROUND_LOCATION_PERMISSION_INDEX = 1
+    const val REQUEST_ACCESS_FINE_LOCATION_CODE = 1590
+}
+
+fun isPermissionGranted(context : Context, permission: String): Boolean {
+    return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+        PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+            context,
+            permission)
+    } else {
+        true
+    }
 }
